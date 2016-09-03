00d85b3e7bf42ae3c9db29c6e20d9bdccf90e557
Anesu Mafuvadze <mafuvadzeanesu@gmail.com>
2016-09-03 02:44:23 -0400

    initial commit

---
+++ app/src/main/java/genieus/com/walla/Profile.java
@@ -0,0 +1,52 @@
+package genieus.com.walla;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class Profile extends AppCompatActivity {
+
+    ListView lv;
+    final String[] settings = new String[]{"My Calendar", "Account Settings", "Logout"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initUi();
+
+    }
+
+    private void initUi(){
+        lv = (ListView) findViewById(R.id.profile_options);
+        SettingsAdapter adapter = new SettingsAdapter(getBaseContext(), R.layout.settings_template, settings);
+        lv.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/genieus/com/walla/SettingsAdapter.java
@@ -0,0 +1,49 @@
+package genieus.com.walla;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Anesu on 9/3/2016.
+ */
+public class SettingsAdapter extends ArrayAdapter<String> {
+    String[] settings;
+    int resource;
+
+    public SettingsAdapter(Context context, int resource, String[] settings) {
+        super(context, resource);
+        this.settings = settings;
+        this.resource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null)
+        {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(resource, parent, false);
+        }
+
+        TextView setting_name = (TextView) convertView.findViewById(R.id.setting_name);
+
+        if(settings[position].equals("Logout")){
+            setting_name.setTextColor(Color.RED);
+            setting_name.setText("Logout");
+        }else{
+            setting_name.setText(settings[position]);
+            setting_name.setTextColor(Color.parseColor("#d3d3d3"));
+        }
+
+        return convertView;
+}
+
+    @Override
+    public int getCount() {
+        return settings.length;
+    }
+}
