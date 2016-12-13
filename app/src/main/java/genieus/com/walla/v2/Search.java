0371d4dd43e28b804592848410c47a091ca2f13c
Anesu Mafuvadze <mafuvadzeanesu@gmail.com>
2016-12-13 02:37:36 -0500

    started v2

---
+++ app/src/main/java/genieus/com/walla/v2/Search.java
@@ -0,0 +1,22 @@
+package genieus.com.walla.v2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import genieus.com.walla.R;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
