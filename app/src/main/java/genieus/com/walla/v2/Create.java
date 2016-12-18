60539cf9bea01afd738d98624a0fd0f15343761b
Anesu Mafuvadze <mafuvadzeanesu@gmail.com>
2016-12-18 18:08:28 -0500

    added time/date picker dialogs

--- app/src/main/java/genieus/com/walla/v2/Create.java
+++ app/src/main/java/genieus/com/walla/v2/Create.java
@@ -1,5 +1,7 @@
 package genieus.com.walla.v2;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,14 +22,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import genieus.com.walla.R;
 
-public class Create extends AppCompatActivity implements OnMapReadyCallback {
+public class Create extends AppCompatActivity implements OnMapReadyCallback, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     private GoogleMap mMap;
+    private TextView start_time;
+    private TextView end_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,40 @@ public class Create extends AppCompatActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        start_time = (TextView) findViewById(R.id.start_time_in);
+        end_time = (TextView) findViewById(R.id.end_time_in);
+
+        start_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog();
+            }
+        });
+
+        end_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog();
+            }
+        });
+    }
+
+    private void showDateDialog(){
+        final java.util.Calendar cal = java.util.Calendar.getInstance();
+        int yr = cal.get(java.util.Calendar.YEAR);
+        int month = cal.get(java.util.Calendar.MONTH);
+        int day = cal.get(java.util.Calendar.DAY_OF_MONTH);
+
+        new DatePickerDialog(this, this, yr, month, day).show();
+    }
+
+    private void showTimeDialog(){
+        final java.util.Calendar cal = java.util.Calendar.getInstance();
+        int h = cal.get(java.util.Calendar.HOUR_OF_DAY);
+        int m = cal.get(java.util.Calendar.MINUTE);
+
+        new TimePickerDialog(this, this, h, m, false).show();
     }
 
     @Override
@@ -54,5 +96,16 @@ public class Create extends AppCompatActivity implements OnMapReadyCallback {
         mMap.addMarker(new MarkerOptions().position(sydney).title("Event location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney ));
         mMap.animateCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        showTimeDialog();
+    }
+
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
     }
 }
