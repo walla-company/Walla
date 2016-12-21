67d6aeb5c55b6877250736859b7ddb7e0914a16e
Anesu Mafuvadze <mafuvadzeanesu@gmail.com>
2016-12-20 23:30:15 -0600

    integrated google places api

--- app/src/main/java/genieus/com/walla/v2/Create.java
+++ app/src/main/java/genieus/com/walla/v2/Create.java
@@ -2,18 +2,30 @@ package genieus.com.walla.v2;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -29,10 +41,16 @@ import java.util.*;
 
 import genieus.com.walla.R;
 
-public class Create extends AppCompatActivity implements OnMapReadyCallback, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+public class Create extends AppCompatActivity implements OnMapReadyCallback, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener, GoogleApiClient.OnConnectionFailedListener {
     private GoogleMap mMap;
     private TextView start_time;
     private TextView end_time;
+    private TextView location;
+    private RelativeLayout map_container;
+
+    private GoogleApiClient mGoogleApiClient;
+    static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    static final String TAG = "Places";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +59,17 @@ public class Create extends AppCompatActivity implements OnMapReadyCallback, Dat
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initGoogleClient();
         initUi();
+    }
+
+    private void initGoogleClient() {
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
     }
 
     private void initUi() {
@@ -52,6 +80,8 @@ public class Create extends AppCompatActivity implements OnMapReadyCallback, Dat
 
         start_time = (TextView) findViewById(R.id.start_time_in);
         end_time = (TextView) findViewById(R.id.end_time_in);
+        location = (TextView) findViewById(R.id.location_in);
+        map_container = (RelativeLayout) findViewById(R.id.location_pin_container);
 
         start_time.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,13 +89,20 @@ public class Create extends AppCompatActivity implements OnMapReadyCallback, Dat
                 showDateDialog();
             }
         });
-
         end_time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDateDialog();
             }
         });
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPlace(location);
+            }
+        });
+
+        map_container.setVisibility(View.GONE);
     }
 
     private void showDateDialog(){
@@ -85,6 +122,13 @@ public class Create extends AppCompatActivity implements OnMapReadyCallback, Dat
         new TimePickerDialog(this, this, h, m, false).show();
     }
 
+    private void setMarker(LatLng place){
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(place , 14);
+        mMap.addMarker(new MarkerOptions().position(place).title("Event location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
+        mMap.animateCamera(cameraUpdate);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -92,10 +136,46 @@ public class Create extends AppCompatActivity implements OnMapReadyCallback, Dat
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
 
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(sydney , 14);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Event location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney ));
-        mMap.animateCamera(cameraUpdate);
+        setMarker(sydney);
+    }
+
+    public void findPlace(View view) {
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                            .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+    }
+
+    // A place has been received; use requestCode to track the request.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                initLocation(place);
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.i(TAG, status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
+    private void initLocation(Place place) {
+        map_container.setVisibility(View.VISIBLE );
+        LatLng loc = place.getLatLng();
+        setMarker(loc);
+        location.setText(place.getName());
     }
 
     @Override
@@ -106,6 +186,11 @@ public class Create extends AppCompatActivity implements OnMapReadyCallback, Dat
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
